45c4dbbf2a0d6f2b82944388a2249309a84eee7c
AndresRey1998 <55194409+AndresRey1998@users.noreply.github.com>
2020-05-02 18:18:05 -0500

    Clase cliente creada

---
+++ src/co/edu/unbosque/model/Cliente.java
@@ -0,0 +1,61 @@
+package co.edu.unbosque.model;
+
+public class Cliente {
+	private String nombres;
+	private String docuemto;
+	private String contraseña;
+	private String celular;
+	private String tipoDocumento;
+	private String apellidos;
+	private String correo;
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getDocuemto() {
+		return docuemto;
+	}
+	public void setDocuemto(String docuemto) {
+		this.docuemto = docuemto;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [nombres=" + nombres + ", docuemto=" + docuemto + ", contraseña=" + contraseña + ", celular="
+				+ celular + ", tipoDocumento=" + tipoDocumento + ", apellidos=" + apellidos + ", correo=" + correo
+				+ "]";
+	}
+	
+
+}
