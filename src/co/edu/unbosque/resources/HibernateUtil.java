85dfd94a853b418b26e2c28f3afa333104885c01
LAURA MARIA LOPEZ MORENO <lopezmlm40@gmail.com>
2020-05-27 16:18:31 -0500

    Documentado

--- src/co/edu/unbosque/resources/HibernateUtil.java
+++ src/co/edu/unbosque/resources/HibernateUtil.java
@@ -1,3 +1,23 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad El Bosque (Bogotá - Colombia)
+ * Programa de Ingeniería de Sistemas
+ * Programación II
+ * 
+ * Profesor: Miguel Alejandro Feijoo García
+ * 
+ * Licenciado bajo el esquema Academic Free License version 2.1 
+ *
+ * Proyecto The Gran Hermano Store
+ * Proyecto Final Grupo C
+ * Autor: Equipo de ElectroCompras Corp:
+ * 	@author	Juan David Alberto Quintero Gaona
+ * 	@author	Laura María López Moreno
+ * 	@author	Andrés Felipe Rey Pedraza
+ * 	@author	Juan Camilo Díaz
+ * 	@author	Camilo Andrés Romero Posada
+ * 			
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ */
 package co.edu.unbosque.resources;
 
 import org.hibernate.Session;
@@ -8,31 +28,48 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * Clase de Hibernate para realizar el proceso de gestion de sesiones.
+ */
 public class HibernateUtil {
 
+	// -----------------------------------------------------------------
+    // Atributos
+    // -----------------------------------------------------------------
+	
+	/**
+	 * Registro.
+	 */
     private static StandardServiceRegistry registry;
 
+    /**
+     * Objeto de SessionFactory.
+     */
     private static SessionFactory sessionFactory;
 
+    /**
+     * Construye la sessionFactoty.
+     * @return una sessionFactory
+     */
     public static SessionFactory getSessionFactory() {
 
         if (sessionFactory == null) {
 
             try {
 
-                // Create registry
+                // Crea el registro.
 
                 registry = new StandardServiceRegistryBuilder().configure().build();
 
-                // Create MetadataSources
+                // Crea el MetadataSources
 
                 MetadataSources sources = new MetadataSources(registry);
 
-                // Create Metadata
+                // Crea el Metadata
 
                 Metadata metadata = sources.getMetadataBuilder().build();
 
-                // Create SessionFactory
+                // Crea el SessionFactory
 
                 sessionFactory = metadata.getSessionFactoryBuilder().build();
 
@@ -54,6 +91,10 @@ public class HibernateUtil {
 
     }
     
+    /**
+     * Devuelve la sesión del hibernate.
+     * @return session
+     */
     public static Session getHibernateSession() {
 
         final SessionFactory sf = new Configuration()
